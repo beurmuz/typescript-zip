2f59da35ec3bb5aec1986f0b40edb5d7658284ec
beurmuz <fallinta2@gmail.com>
2025-08-19 18:10:55 +0900

    #2 Generic과 Promise

---
+++ onebite/understand/src/generic-ch4.ts
@@ -0,0 +1,50 @@
+/**
+ * 제네릭 클래스
+ */
+
+class List<T> {
+  constructor(private list: T[]) {}
+
+  push(data: T) {
+    this.list.push(data);
+  }
+
+  pop() {
+    return this.list.pop();
+  }
+
+  print() {
+    console.log(this.list);
+  }
+}
+
+const numberList = new List([1, 2, 3]);
+numberList.pop();
+numberList.push(4);
+numberList.print();
+
+const stringList = new List(["a", "b", "c"]);
+stringList.pop();
+stringList.push("7");
+stringList.print();
+
+// class NumberList {
+//   constructor(private list: number[]) {}
+
+//   push(data: number) {
+//     this.list.push(data);
+//   }
+
+//   pop() {
+//     return this.list.pop();
+//   }
+
+//   print() {
+//     console.log(this.list);
+//   }
+// }
+
+// const numberList = new NumberList([1, 2, 3]);
+// numberList.pop();
+// numberList.push(4);
+// numberList.print();
